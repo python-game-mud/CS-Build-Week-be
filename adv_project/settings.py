de5a1aa113d0effc414a69f74040097409ef5cd2
Josh <josh_hill15@me.com>
2020-04-27 18:12:31 -0400

    heroku

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -16,8 +16,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SITE_ID = 1
-# site = Site.objects.get(id=settings.SITE_ID)
+SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
