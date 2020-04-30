93336e2c5bc69b99a4ea44b142a820b0d68a89eb
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-30 09:53:21 -0700

    import Room from models

--- util/sample_generator2.py
+++ util/sample_generator2.py
@@ -1,29 +1,29 @@
 
 from django.db import models
-# from adventure.models import Room
+from adventure.models import Room
 
 
 
-class Room(models.Model):
-    title = models.CharField(max_length=50, default="DEFAULT TITLE")
-    description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION")
-    n_to = models.IntegerField(default=0)
-    s_to = models.IntegerField(default=0)
-    e_to = models.IntegerField(default=0)
-    w_to = models.IntegerField(default=0)
-    # adding coordinates to rooms
-    x = models.IntegerField(default=0)
-    y = models.IntegerField(default=0)
+# class Room(models.Model):
+#     title = models.CharField(max_length=50, default="DEFAULT TITLE")
+#     description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION")
+#     n_to = models.IntegerField(default=0)
+#     s_to = models.IntegerField(default=0)
+#     e_to = models.IntegerField(default=0)
+#     w_to = models.IntegerField(default=0)
+#     # adding coordinates to rooms
+#     x = models.IntegerField(default=0)
+#     y = models.IntegerField(default=0)
 
-    def connect_rooms(self, connecting_room, direction):
-        reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-        reverse_dir = reverse_dirs[direction]
-        setattr(self, f"{direction}_to", connecting_room.id)
-        setattr(connecting_room, f"{reverse_dir}_to", self.id)
-    def playerNames(self, currentPlayerID):
-        return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
-    def playerUUIDs(self, currentPlayerID):
-        return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+#     def connect_rooms(self, connecting_room, direction):
+#         reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
+#         reverse_dir = reverse_dirs[direction]
+#         setattr(self, f"{direction}_to", connecting_room.id)
+#         setattr(connecting_room, f"{reverse_dir}_to", self.id)
+#     def playerNames(self, currentPlayerID):
+#         return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+#     def playerUUIDs(self, currentPlayerID):
+#         return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
 
 
 
